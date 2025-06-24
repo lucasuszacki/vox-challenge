f2f9308f8886074fb06edeb5a17d37febfdd7a69
Lucas Uszacki <lucasuszacki.wow@gmail.com>
2025-06-24 17:53:31 -0300

    fix: missed modal imports

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,15 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { appRouting } from './app/app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { importProvidersFrom } from '@angular/core';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { ModalModule } from 'ngx-bootstrap/modal';
 
 bootstrapApplication(AppComponent, {
-  providers: [appRouting, provideHttpClient()],
+  providers: [
+    appRouting,
+    provideHttpClient(),
+    provideAnimations(),
+    importProvidersFrom(ModalModule.forRoot()),
+  ],
 }).catch((err) => console.error(err));
